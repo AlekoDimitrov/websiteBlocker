eb79d85938e51785666d3493f4f6fa2686ec1e7b
Aleko Dimitrov <77157358+AlekoDimitrov@users.noreply.github.com>
2021-09-06 21:51:47 +0300

    functional app
    program has full functionality and does what it was intendet. Code needs optimization

--- blocker.py
+++ blocker.py
@@ -1,13 +1,13 @@
 
 websites = ['facebook.com', 'www.facebook.com', 'reddit.com', 'www.reddit.com']
-hosts_path = '/home/alekodimitrov/Documents/hosts'
+hosts_path = '/etc/hosts'
 
 
 def listToString(list):
     str1 = ''
 
     for i in list:
-        str1 += i + '\n'
+        str1 += '127.0.0.1 ' + i + '\n'
     
     return str1
 
@@ -15,12 +15,17 @@ def listToString(list):
 def websiteBlocker():
     with open(hosts_path, 'r+') as f:
         f_contents = f.read()
-        print(f_contents)
         
         if listToString(websites) in f_contents:
-            print('It is there')
+            print('Unblocking sites...')
+            f.seek(0)
+            f.truncate()
+            f.write('127.0.0.1   localhost localhost.localdomain '
+                    'localhost4 localhost4.localdomain4\n'
+                    '::1         localhost localhost.localdomain ' 
+                    'localhost6 localhost6.localdomain6\n')
         else:
-            print("Aren't there")
+            print("Blocking sites...")
             f.write(listToString(websites))
 
 
